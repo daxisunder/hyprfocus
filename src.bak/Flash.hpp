84f7b33e02b9f1611ebadbcf9cdc32f1086c8efd
daxisunder <no-prophet@proton.me>
2026-02-07 17:32:57 +0100

    remove src.bak

--- src.bak/Flash.hpp
+++
@@ -1,8 +0,0 @@
-#include "IFocusAnimation.hpp"
-
-class CFlash : public IFocusAnimation {
-public:
-  void onWindowFocus(PHLWINDOW pWindow, HANDLE pHandle) override;
-  void init(HANDLE pHandle, std::string animationName) override;
-  void setup(HANDLE pHandle, std::string animationName) override;
-};
